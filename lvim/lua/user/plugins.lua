a1bf5a1f67009e77d5559d9526001ec3b4be7339
Jon SUgar <jonathansugar@mac.com>
2022-10-01 12:03:22 +0100

    remove minimap

--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -5,16 +5,6 @@
 lvim.plugins = {
   { 'projekt0n/github-nvim-theme' },
   { 'dracula/vim' },
-  {
-    'wfxr/minimap.vim',
-    run = "cargo install --locked code-minimap",
-    cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
-    config = function()
-      vim.cmd("let g:minimap_width = 10")
-      vim.cmd("let g:minimap_auto_start = 1")
-      vim.cmd("let g:minimap_auto_start_win_enter = 1")
-    end,
-  },
   { 'tpope/vim-repeat' },
   {
     "tpope/vim-surround",
