c56433956e9eef3e02aa2f09ba1e4d875b310587
Jon Sugar <jonathansugar@mac.com>
2022-10-28 09:44:07 +0100

    feat: configure Lunarvim

--- lvim/lua/user/status.lua
+++ lvim/lua/user/status.lua
@@ -4,20 +4,21 @@
 local components = require("lvim.core.lualine.components")
 
 lvim.builtin.lualine.sections.lualine_a = {
-  components.mode,
+  -- components.mode,
   "mode"
 }
 lvim.builtin.lualine.sections.lualine_b = {
-  components.branch,
+  -- components.branch,
+  "branch"
 }
 lvim.builtin.lualine.sections.lualine_c = {
-  components.diff,
+  "diff",
   components.diagnostics,
 }
 
 lvim.builtin.lualine.sections.lualine_x = {
-  components.encoding,
-  components.filetype,
+  "encoding",
+  "filetype",
   components.spaces,
 }
 lvim.builtin.lualine.sections.lualine_y = { components.location }
