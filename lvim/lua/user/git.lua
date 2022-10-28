429f020202ecaaa185c6848c7586634c213ec4be
Jon Sugar <jonathansugar@mac.com>
2022-10-28 09:47:11 +0100

    feat: configure git signs

--- lvim/lua/user/git.lua
+++ lvim/lua/user/git.lua
@@ -2,8 +2,8 @@
 -- ┃ Git                                                                        ┃
 -- ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
-lvim.builtin.gitsigns.opts.signs.add.text = ""
-lvim.builtin.gitsigns.opts.signs.change.text = ""
-lvim.builtin.gitsigns.opts.signs.changedelete.text = ""
-lvim.builtin.gitsigns.opts.signs.delete.text = ""
-lvim.builtin.gitsigns.opts.signs.topdelete.text = ""
+lvim.builtin.gitsigns.opts.signs.add.text = "A"
+lvim.builtin.gitsigns.opts.signs.change.text = "M"
+lvim.builtin.gitsigns.opts.signs.changedelete.text = "C"
+lvim.builtin.gitsigns.opts.signs.delete.text = "D"
+lvim.builtin.gitsigns.opts.signs.topdelete.text = "T"
