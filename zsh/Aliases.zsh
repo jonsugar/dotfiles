9bb7933a1e653e6a6202a2623ca0998dc7259d06
Jon Sugar <jonathansugar@mac.com>
2022-12-05 10:06:47 +0000

    ✨ Override default grep aliases

--- zsh/Aliases.zsh
+++ zsh/Aliases.zsh
@@ -5,9 +5,19 @@
 # Remove conflicting aliases provided by Oh-My-Zsh
 unalias lsa
 unalias l
+unalias grep
+unalias fgrep
+unalias egrep
+
+# Fix "" bug in Fig
+# not sure where this came from but I remember it's needed
+unalias -- -
 
 alias ls='lsd'
 alias tree='broot'
+alias grep='ggrep'
+alias fgrep='ggrep -F'
+alias egrep='ggrep -E'
 alias nv='br --only-folders'
 alias ip='curl http://ipecho.net/plain; echo'
 alias edit:zshrc='$EDITOR ~/.zshrc'
@@ -15,7 +25,7 @@ alias env:find='env | fgrep'
 alias clear:History='echo "" > ~/.zsh_history'
 
 function alias:ls {
-    alias | grep -F --color=never "$1" | awk '{FS = "="; printf("%-10s %s\n",$1,$2) }'
+    alias | ggrep -F "$1" | awk '{FS = "="; printf("[ %10s ] %s\n",$1,$2) }'
 }
 
 alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
